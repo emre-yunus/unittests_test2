8e9636c5c72a4a90bd840c01287fb4765393151e
emre-yunus <r0664998@student.thomasmore.be>
2021-10-18 14:32:52 +0200

    1. for loop zit nu in aparte methode.

--- src/main/java/be/friends/FriendshipUpdater.java
+++ src/main/java/be/friends/FriendshipUpdater.java
@@ -10,33 +10,37 @@ public class FriendshipUpdater {
 
     public void update() {
         for (Friend friend : friends) {
-            boolean wasLucky = !friend.isLucky;
-            if (friend.isLucky && friend.didSomeInteractionToday) {
-                friend.isLucky = !friend.isLucky;
-            }
-            if (!wasLucky)
-                friend.friendshipLevel += 4;
-            if (friend.didSomeInteractionToday) {
-                if (friend.nrOfStars == 0)
-                    friend.nrOfStars++;
-                else {
-                    if (friend.friendshipLevel == 9) {
-                        if (friend.nrOfStars == 1) {
-                            friend.friendshipLevel = -1;
-                            friend.nrOfStars = 2;
-                        }
+            friendUpdate(friend);
+        }
+    }
+
+    private void friendUpdate(Friend friend) {
+        boolean wasLucky = !friend.isLucky;
+        if (friend.isLucky && friend.didSomeInteractionToday) {
+            friend.isLucky = !friend.isLucky;
+        }
+        if (!wasLucky)
+            friend.friendshipLevel += 4;
+        if (friend.didSomeInteractionToday) {
+            if (friend.nrOfStars == 0)
+                friend.nrOfStars++;
+            else {
+                if (friend.friendshipLevel == 9) {
+                    if (friend.nrOfStars == 1) {
+                        friend.friendshipLevel = -1;
+                        friend.nrOfStars = 2;
                     }
-                    friend.friendshipLevel++;
                 }
-                if (friend.nrOfStars != 3 && friend.friendshipLevel == 30) {
-                    friend.friendshipLevel = 0;
-                    friend.nrOfStars = friend.nrOfStars + 1;
-                }
+                friend.friendshipLevel++;
             }
-            if (!wasLucky && !friend.didSomeInteractionToday) {
-                friend.isLucky = true;
-                friend.friendshipLevel -= 4;
+            if (friend.nrOfStars != 3 && friend.friendshipLevel == 30) {
+                friend.friendshipLevel = 0;
+                friend.nrOfStars = friend.nrOfStars + 1;
             }
+        }
+        if (!wasLucky && !friend.didSomeInteractionToday) {
+            friend.isLucky = true;
+            friend.friendshipLevel -= 4;
         }
     }
 }
