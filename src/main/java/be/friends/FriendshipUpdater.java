0a50069e53c9fba6adc82ac69ca03613a6bbefbc
emre-yunus <r0664998@student.thomasmore.be>
2021-10-18 15:53:58 +0200

    17. Toename van star is nu in aparte methode

--- src/main/java/be/friends/FriendshipUpdater.java
+++ src/main/java/be/friends/FriendshipUpdater.java
@@ -18,21 +18,25 @@ public class FriendshipUpdater {
         if (friend.didSomeInteractionToday) {
             friend.friendshipLevel++;
 
-            if (friend.nrOfStars == 0) {
-                friend.friendshipLevel = 0;
-                friend.nrOfStars++;
-            } else if(friend.friendshipLevel == 10 && friend.nrOfStars == 1){
-                friend.friendshipLevel = 0;
-                friend.nrOfStars++;
-            } else if(friend.nrOfStars == 2 && friend.friendshipLevel == 30) {
-                friend.friendshipLevel = 0;
-                friend.nrOfStars++;
-            }
+            updateIncreaseStar(friend);
 
             if(friend.isLucky) {
                 friend.isLucky = false;
                 friend.friendshipLevel +=4;
             }
+        }
+    }
+
+    private void updateIncreaseStar(Friend friend) {
+        if (friend.nrOfStars == 0) {
+            friend.friendshipLevel = 0;
+            friend.nrOfStars++;
+        } else if(friend.friendshipLevel == 10 && friend.nrOfStars == 1){
+            friend.friendshipLevel = 0;
+            friend.nrOfStars++;
+        } else if(friend.nrOfStars == 2 && friend.friendshipLevel == 30) {
+            friend.friendshipLevel = 0;
+            friend.nrOfStars++;
         }
     }
 }
